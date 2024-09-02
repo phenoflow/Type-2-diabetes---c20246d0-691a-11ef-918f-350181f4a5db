0c75b70932506e9f90e8a27dc6f93d405b5a892c
Martin Chapman <contact@martinchapman.co.uk>
2024-09-02 12:03:53 +0100

    Update made by Phenoflow

---
+++ python/type-2-diabetes-circulatory---icduncat.py
@@ -0,0 +1,25 @@
+# phekb, 2024.
+
+import sys, csv, re
+
+codes = [{"code":"E11.51","system":"icduncat"},{"code":"E11.52","system":"icduncat"},{"code":"E11.59","system":"icduncat"}];
+REQUIRED_CODES = 1;
+with open(sys.argv[1], 'r') as file_in, open('type-2-diabetes-potential-cases.csv', 'w', newline='') as file_out:
+    csv_reader = csv.DictReader(file_in)
+    csv_writer = csv.DictWriter(file_out, csv_reader.fieldnames + ["type-2-diabetes-circulatory---icduncat-identified"])
+    csv_writer.writeheader();
+    codes_identified = 0;
+    for row in csv_reader:
+        newRow = row.copy();
+        for cell in row:
+            # Iterate cell lists (e.g. codes)
+            for item in re.findall(r'\(([^,]*)\,', row[cell]):
+                if(item in list(map(lambda code: code['code'], codes))): codes_identified+=1;
+                if(codes_identified>=REQUIRED_CODES):
+                    newRow["type-2-diabetes-circulatory---icduncat-identified"] = "CASE";
+                    break;
+            if(codes_identified>=REQUIRED_CODES): break;
+        if(codes_identified<REQUIRED_CODES):
+            newRow["type-2-diabetes-circulatory---icduncat-identified"] = "UNK";
+        codes_identified=0;
+        csv_writer.writerow(newRow)
